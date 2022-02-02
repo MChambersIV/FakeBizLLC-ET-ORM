69115f17b87ceca3a737dcb133a03a9fa43746b9
MChambersIV <chambersm025@gmail.com>
2022-02-01 21:40:17 -0800

    added base files and begun code for schema.sql, seeds.sql, and server.js

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO departments (name)
+VALUES  ("Administration"),
+        ("Marketing"),
+        ("Legal"),
+        ("Human Resources"),
+        ("Engineering");
+
+INSERT INTO roles (name)
+VALUES  ("Administrator"),
+        ("Marketing Lead"),
+        ("Lawyer"),
+        ("HR Generalist"),
+        ("Engineer");
+
+INSERT INTO employee_first (name)
+VALUES  ("Steve"),
+        ("Dina"),
+        ("Matthew"),
+        ("Sarah"),
+        ("Frances");
+
+INSERT INTO employee_last (name)
+VALUES  ("Brule"),
+        ("Gumby"),
+        ("Flooper"),
+        ("MacIntosh"),
+        ("Manances");
