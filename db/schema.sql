3dd841d33293dc1a5edf99e8919b1f3fbe5d3cde
MChambersIV <chambersm025@gmail.com>
2022-02-04 10:18:34 -0800

    updated schema.sql to properly reflect insertion order

--- db/schema.sql
+++ db/schema.sql
@@ -10,18 +10,16 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     department_id INT,
     salary DECIMAL NOT NULL,
     FOREIGN KEY (department_id)
-    REFERENCES departments(id),
-    PRIMARY KEY (id)
+    REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    role_id INT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     FOREIGN KEY (role_id)
